344416121326bcbaae2013e531059d456158c47d
Hermanoid <lucasniewohner@gmail.com>
2024-02-05 22:03:10 -0700

    More work on basic ICP

--- hw2/icp/include/icp.h
+++ hw2/icp/include/icp.h
@@ -5,5 +5,7 @@ using Eigen::Vector3d;
 using std::vector;
 using Eigen::Isometry3d;
 
+using PointCloud=Eigen::Matrix<double, 3, Eigen::Dynamic>;
+
 // An alias for Eigen::Transform<double, 3, Eigen::Affine>
-Isometry3d icp(const vector<Vector3d> &target, const vector<Vector3d> &source, int max_iter=100);
+Isometry3d icp(const PointCloud &target, const PointCloud &source, int max_iter);
