9a76da30f994737e8567113f106af438e5710d51
Lucas Niewohner <lucasniewohner@gmail.com>
2024-03-06 10:25:45 -0700

    Created 8-pt folder

--- hw2/README.md
+++
@@ -1,1 +0,0 @@
-Useful reference: [Simple ICP GitHub](https://github.com/pglira/simpleICP/blob/master/c%2B%2B/src/simpleicp.cpp)
--- hw2/icp/src/icp.cpp
+++ hw2/icp/src/icp.cpp
@@ -1,6 +1,14 @@
 #include "icp.h"
+#include <Eigen/Dense>
+#include <Eigen/Jacobi>
+#include <iostream>
+#include <nanoflann.hpp>
+#include <pthread.h>
+
+
+
 
 Isometry3d icp(const vector<Vector3d> &target, const vector<Vector3d> &source, int max_iter){
-    // Your code here
+    
     return Isometry3d::Identity();
 }
